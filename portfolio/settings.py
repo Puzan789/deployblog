76139341887974228daf6c8bd8adf155affa2a22
puzan789 <puzan936@gmail.com>
2023-02-16 12:37:33 +0545

    updates

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ou5tj_ouibw58&)+jvpkgrum1snf_hbd!@&$0a0lbdbg-oxgnx"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
