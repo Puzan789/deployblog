941f543595aee38c2a0d7d38b72e2d409e8a429d
puzan789 <puzan936@gmail.com>
2023-03-11 21:58:23 +0545

    updates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
-
 from django.shortcuts import render
 from .models import project
-# Create your views here.
-def home(request):
-    pro=project.objects.all()
-    return render(request, 'blog/index.html',{'pro':pro})
+from django.views.decorators.cache import never_cache
 
-def error_404(request,exception):
-    return render(request, 'blog/404.html')
+# Create your views here.
+@never_cache
+def home(request):
+    visits = request.session.get("visits", 0)
+    request.session["visits"] = visits + 1
+    message = f"Number of visits: {visits + 1}"
+    pro = project.objects.all()
+    return render(request, "blog/index.html", {"pro": pro, "message": message})
+
+
+def error_404(request, exception):
+    return render(request, "blog/404.html")
